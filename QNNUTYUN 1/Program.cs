8f4c98aecce6cb8990c0061e6142718abd8bc94c
Student <Student@VTC6-PC>
2023-06-04 19:24:54 +0400

    Add project files.

---
+++ QNNUTYUN 1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace QNNUTYUN_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            while (number >=50 && number <= 70)
+            {
+                while (number != 1)
+                {
+
+
+
+                    if (number % 2 == 0)
+                    {
+                        number /= 2;
+
+                    }
+                    else
+                    {
+                        number = ((number * 3) + 1) / 2;
+                    }
+
+                    Console.WriteLine(number);
+
+
+                }
+                
+                
+                    
+                
+            }
+            
+            
+            
+
+        }
+    }
+}
